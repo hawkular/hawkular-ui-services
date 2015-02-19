cf65eeb2a2c4b45e01ca2dc070926cd8480ba868
Viliam Rockai <vrockai@redhat.com>
2015-02-19 16:28:58 +0100

    Added license to alerts

--- dist/hawkular-ui-service.js
+++ dist/hawkular-ui-service.js
@@ -19,6 +19,165 @@ var hawkularRest;
 
 var hawkularRest;
 (function (hawkularRest) {
+    hawkularRest._module.provider('HawkularAlert', function () {
+        this.host = 'localhost';
+        this.port = 8080;
+        this.setHost = function (host) {
+            this.host = host;
+            return this;
+        };
+        this.setPort = function (port) {
+            this.port = port;
+            return this;
+        };
+        this.$get = ['$resource', function ($resource) {
+            var prefix = 'http://' + this.host + ':' + this.port;
+            var factory = {};
+            factory.Alert = $resource(prefix + '/hawkular/alerts', {}, {
+                reload: {
+                    method: 'GET',
+                    url: prefix + '/hawkular/alerts/reload'
+                }
+            });
+            factory.Trigger = $resource(prefix + '/hawkular/alerts/triggers/:triggerId', {
+                triggerId: '@triggerId'
+            }, {
+                save: {
+                    method: 'POST',
+                    url: prefix + '/hawkular/alerts/triggers/'
+                },
+                put: {
+                    method: 'PUT'
+                },
+                conditions: {
+                    method: 'GET',
+                    url: prefix + '/hawkular/alerts/triggers/:triggerId/conditions',
+                    isArray: true,
+                    params: {
+                        triggerId: '@triggerId'
+                    }
+                }
+            });
+            factory.Dampening = $resource(prefix + '/hawkular/alerts/trigger/dampening/:triggerId', {
+                triggerId: '@triggerId'
+            }, {
+                save: {
+                    method: 'POST',
+                    url: prefix + '/hawkular/alerts/trigger/dampening/'
+                },
+                put: {
+                    method: 'PUT'
+                }
+            });
+            factory.AvailabilityCondition = $resource(prefix + '/hawkular/alerts/conditions/availability/:conditionId', {
+                conditionId: '@conditionId'
+            }, {
+                put: {
+                    method: 'PUT'
+                },
+                trigger: {
+                    method: 'GET',
+                    url: prefix + '/hawkular/alerts/conditions/availability/trigger/:triggerId',
+                    isArray: true,
+                    params: {
+                        triggerId: '@triggerId'
+                    }
+                }
+            });
+            factory.CompareCondition = $resource(prefix + '/hawkular/alerts/conditions/compare/:conditionId', {
+                conditionId: '@conditionId'
+            }, {
+                put: {
+                    method: 'PUT'
+                },
+                trigger: {
+                    method: 'GET',
+                    url: prefix + '/hawkular/alerts/conditions/compare/trigger/:triggerId',
+                    isArray: true,
+                    params: {
+                        triggerId: '@triggerId'
+                    }
+                }
+            });
+            factory.StringCondition = $resource(prefix + '/hawkular/alerts/conditions/string/:conditionId', {
+                conditionId: '@conditionId'
+            }, {
+                put: {
+                    method: 'PUT'
+                },
+                trigger: {
+                    method: 'GET',
+                    url: prefix + '/hawkular/alerts/conditions/string/trigger/:triggerId',
+                    isArray: true,
+                    params: {
+                        triggerId: '@triggerId'
+                    }
+                }
+            });
+            factory.ThresholdCondition = $resource(prefix + '/hawkular/alerts/conditions/threshold/:conditionId', {
+                conditionId: '@conditionId'
+            }, {
+                put: {
+                    method: 'PUT'
+                },
+                trigger: {
+                    method: 'GET',
+                    url: prefix + '/hawkular/alerts/conditions/threshold/trigger/:triggerId',
+                    isArray: true,
+                    params: {
+                        triggerId: '@triggerId'
+                    }
+                }
+            });
+            factory.ThresholdRangeCondition = $resource(prefix + '/hawkular/alerts/conditions/range/:conditionId', {
+                conditionId: '@conditionId'
+            }, {
+                put: {
+                    method: 'PUT'
+                },
+                trigger: {
+                    method: 'GET',
+                    url: prefix + '/hawkular/alerts/conditions/range/trigger/:triggerId',
+                    isArray: true,
+                    params: {
+                        triggerId: '@triggerId'
+                    }
+                }
+            });
+            factory.NotifierType = $resource(prefix + '/hawkular/alerts/notifierType/:notifierType', {
+                notifierType: '@notifierType'
+            }, {
+                get: {
+                    method: 'GET',
+                    isArray: true
+                }
+            });
+            factory.Notifier = $resource(prefix + '/hawkular/alerts/notifiers/:notifierId', {
+                notifierId: '@notifierId'
+            }, {
+                put: {
+                    method: 'PUT',
+                    url: prefix + '/hawkular/alerts/notifiers/:notifierId',
+                    params: {
+                        notifierId: '@notifierId'
+                    }
+                },
+                notifierType: {
+                    method: 'GET',
+                    isArray: true,
+                    url: prefix + '/hawkular/alerts/notifiers/type/:notifierType',
+                    params: {
+                        notifierType: '@notifierType'
+                    }
+                }
+            });
+            return factory;
+        }];
+    });
+})(hawkularRest || (hawkularRest = {}));
+
+var hawkularRest;
+(function (hawkularRest) {
     hawkularRest._module.provider('HawkularInventory', function () {
         this.host = 'localhost';
         this.port = 8080;
